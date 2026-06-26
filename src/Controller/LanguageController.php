77119f773794b7e0a927aef37bb7767d39a6de2a
Soner Yurt <245046418+full-protected@users.noreply.github.com>
2026-06-26 07:15:00 +0200

    initialize msg

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class LanguageController extends AbstractController
 {
-    #[Route('/language', name: 'app_language')]
-    public function index(): Response
+    #[Route('/language/{locale}', name: 'app_language')]
+    public function switchLanguage(string $locale, Request $request): Response
     {
-        return $this->render('language/index.html.twig', [
-            'controller_name' => 'LanguageController',
-        ]);
+        if (!in_array($locale, ['fr', 'en'])) {
+            $locale = 'en';
+        }
+
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirect($request->headers->get('referer', '/'));
     }
 }
