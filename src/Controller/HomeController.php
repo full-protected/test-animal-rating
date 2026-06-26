9ffea58f325db1b55ef3c2f6659748f81fc802e8
Soner Yurt <245046418+full-protected@users.noreply.github.com>
2026-06-26 06:09:59 +0200

    feat: create submission form with AnimalRatingType

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\AnimalRating;
+use App\Form\AnimalRatingType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $animalRating = new AnimalRating();
+        $form = $this->createForm(AnimalRatingType::class, $animalRating);
+        $form->handleRequest($request);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form,
         ]);
     }
 }
