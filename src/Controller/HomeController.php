fa1c19c7bf155db3847ecdcac430a7c4dff8bf86
Soner Yurt <245046418+full-protected@users.noreply.github.com>
2026-06-26 06:42:15 +0200

    refactor: extract save logic into AnimalRatingService

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,7 @@ namespace App\Controller;
 
 use App\Entity\AnimalRating;
 use App\Form\AnimalRatingType;
-use App\Repository\AnimalRatingRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\AnimalRatingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,33 +13,18 @@ use Symfony\Component\HttpFoundation\Request;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, EntityManagerInterface $em, AnimalRatingRepository $repository): Response
+    public function index(Request $request, AnimalRatingService $animalRatingService): Response
     {
         $animalRating = new AnimalRating();
         $form = $this->createForm(AnimalRatingType::class, $animalRating);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Normalize userName & animalName
-            $animalRating->setUserName(trim($animalRating->getUserName()));
-            $animalRating->setAnimalName(strtolower(trim($animalRating->getAnimalName())));
-
-            // Check if an animal is existing for this username
-            $existing = $repository->findOneBy(['userName' => $animalRating->getUserName()]);
-
-            if ($existing) {
-                $existing->setAnimalName($animalRating->getAnimalName());
-                $existing->setScore($animalRating->getScore());
-            } else {
-                $em->persist($animalRating);
-            }
-
-            $em->flush();
-
+            $animalRatingService->save($animalRating);
             $this->addFlash('success', 'Votre animal a bien été enregistré !');
-
             return $this->redirectToRoute('app_home');
         }
+
 
         return $this->render('home/index.html.twig', [
             'form' => $form,
