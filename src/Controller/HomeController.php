b0a0a6d5bf5c093268410115281e6372b52eeacf
Soner Yurt <245046418+full-protected@users.noreply.github.com>
2026-06-26 06:27:06 +0200

    handle submission of form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\AnimalRating;
 use App\Form\AnimalRatingType;
+use App\Repository\AnimalRatingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,11 +14,20 @@ use Symfony\Component\HttpFoundation\Request;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
         $animalRating = new AnimalRating();
         $form = $this->createForm(AnimalRatingType::class, $animalRating);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($animalRating);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre animal a bien été enregistré !');
+
+            return $this->redirectToRoute('app_home');
+        }
 
         return $this->render('home/index.html.twig', [
             'form' => $form,
